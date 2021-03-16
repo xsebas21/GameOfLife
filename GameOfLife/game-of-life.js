0dc8e96d15b1e63b4f928ced70b65b0fdc851270
xsebas21 <xsebas21@gmail.com>
2021-03-16 02:49:06 -0300

    Add project files.

---
+++ GameOfLife/game-of-life.js
@@ -0,0 +1,30 @@
+﻿///// Begin of Configs
+var rows = 50;
+var columns = 80;
+var initialAliveCells = [
+    "5-5", "5-6",
+    "6-5", "6-6"
+];
+
+///// End of Configs
+
+var game = {};
+
+game.draw = function () {
+    var grid = document.getElementById("grid");
+
+    for (var i = 0; i < rows; i++) {
+        var row = grid.insertRow();
+        row.className = "gridRow";
+
+        for (var j = 0; j < columns; j++) {
+            var cell = row.insertCell();
+            cell.id = i + "-" + j;
+            cell.className = "cell";
+
+            if (initialAliveCells.indexOf(cell.id) > -1) {
+                cell.classList.add("alive");
+            }
+        }
+    }
+};
